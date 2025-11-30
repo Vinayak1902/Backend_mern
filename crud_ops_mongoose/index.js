c3d552392dced1078476ac75676605c36ae2a99e
Vinayak Agrawal <vinayakagrawal837@gmail.com>
2025-11-30 18:59:06 +0530

    adding crud operations using mongoose and model

--- crud_ops_mongoose/index.js
+++ crud_ops_mongoose/index.js
@@ -20,10 +20,30 @@ app.post('/api/enquiry-insert',(req,res)=>{
             status:1,
             message:"Enquiry saved successfully"});
         }).catch((err)=>{
-            res.send({status:0,message:"Error while saving enquiry"},err);
+            res.send({status:0,message:"Error while saving enquiry",error:err});
         })
 })
-
+app.get("/api/enquiry-list",async (req,res)=>{
+    let enquiryList = await enquiryModel.find();
+    res.status(200).json({status:1, message:"Enquiry list", data:enquiryList})
+})
+app.delete("/api/enquiry-delete/:id",async (req,res)=>{
+    let enquiryId = req.params.id;
+    let deletedEnquiry = await enquiryModel.deleteOne({_id:enquiryId});
+    res.send({status:1,message:"Enquiry deleted successfully",id:enquiryId,delRes:deletedEnquiry})
+})
+app.put("/api/enquiry-update/:id",async (req,res)=>{
+    let enquiryId = req.params.id;
+    let {sName,sEmail,sPhone,sMessage} = req.body;
+    let updatedEnquiry = {
+        name:sName,
+        email:sEmail,
+        phone:sPhone,
+        message:sMessage
+    };
+    let updateRes = await enquiryModel.updateOne({_id:enquiryId},updatedEnquiry);
+    res.send({status:1,message:"Enquiry updated successfully",id:enquiryId,updateRes:updateRes})
+    })
 mongoose.connect(process.env.DBURL).then(()=>{
     console.log("Connected to MongoDB");
     app.listen(process.env.PORT,()=>{
