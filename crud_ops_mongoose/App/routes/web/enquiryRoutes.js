bfa97b643146a61c25938129a2cc21025a4235e0
Vinayak Agrawal <vinayakagrawal837@gmail.com>
2025-12-01 21:10:34 +0530

    modified the crud operation to use mvc pattern

---
+++ crud_ops_mongoose/App/routes/web/enquiryRoutes.js
@@ -0,0 +1,8 @@
+let express = require('express');
+const { enquiryDelete, enquiryInsert, enquiryList, enquiryUpdate } = require('../../controller/admin/web/userEnquiryController');
+let enquiryRoutes = express.Router();
+enquiryRoutes.post('/enquiry-insert',enquiryInsert)
+enquiryRoutes.get("/enquiry-list",enquiryList)
+enquiryRoutes.delete("/enquiry-delete/:id",enquiryDelete)
+enquiryRoutes.put("/enquiry-update/:id",enquiryUpdate)
+module.exports = enquiryRoutes;
--- crud_ops_mongoose/index.js
+++ crud_ops_mongoose/index.js
@@ -1,49 +1,13 @@
 let express = require('express');
 var mongoose = require('mongoose');
 require('dotenv').config();
-let enquiryModel = require('./models/enquiry.model');
+const enquiryRoutes = require('./App/routes/web/enquiryRoutes');
 
 // Connect to MongoDB
 
 let app = express();
 app.use(express.json());
-app.post('/api/enquiry-insert',(req,res)=>{
-    let {sName,sEmail,sPhone,sMessage} = req.body;
-    let enquiry = new enquiryModel({
-        name:sName,
-        email:sEmail,
-        phone:sPhone,
-        message:sMessage
-    });
-    enquiry.save().then(()=>{
-        res.send({
-            status:1,
-            message:"Enquiry saved successfully"});
-        }).catch((err)=>{
-            res.send({status:0,message:"Error while saving enquiry",error:err});
-        })
-})
-app.get("/api/enquiry-list",async (req,res)=>{
-    let enquiryList = await enquiryModel.find();
-    res.status(200).json({status:1, message:"Enquiry list", data:enquiryList})
-})
-app.delete("/api/enquiry-delete/:id",async (req,res)=>{
-    let enquiryId = req.params.id;
-    let deletedEnquiry = await enquiryModel.deleteOne({_id:enquiryId});
-    res.send({status:1,message:"Enquiry deleted successfully",id:enquiryId,delRes:deletedEnquiry})
-})
-app.put("/api/enquiry-update/:id",async (req,res)=>{
-    let enquiryId = req.params.id;
-    let {sName,sEmail,sPhone,sMessage} = req.body;
-    let updatedEnquiry = {
-        name:sName,
-        email:sEmail,
-        phone:sPhone,
-        message:sMessage
-    };
-    let updateRes = await enquiryModel.updateOne({_id:enquiryId},updatedEnquiry);
-    res.send({status:1,message:"Enquiry updated successfully",id:enquiryId,updateRes:updateRes})
-    })
+app.use("/web/api/enquiry",enquiryRoutes)
 mongoose.connect(process.env.DBURL).then(()=>{
     console.log("Connected to MongoDB");
     app.listen(process.env.PORT,()=>{
